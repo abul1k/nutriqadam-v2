eb1986866c822c210b7a4e9326c50075416f9ddf
Abdulaziz <abulacoder@gmail.com>
2025-11-30 16:16:39 +0500

    footer padding bug

--- app/page.js
+++ app/page.js
@@ -742,7 +742,7 @@ export default function NutriQadamApp() {
     <div className='min-h-screen bg-gray-50'>
       <Navigation />
 
-      <div className='flex'>
+      <div className='flex lg:pb-0 pb-20'>
         <Sidebar />
 
         <main className='flex-1 p-4 lg:p-8'>
